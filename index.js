80b560544196c7323434cd34ef4ae94345b0d5d0
KakushaVenecia <kakushavenecia@gmail.com>
2023-02-23 06:05:50 -0800

    This will add the doc files

---
+++ index.js
@@ -0,0 +1,103 @@
+// Question 1 & 2 
+// ARITHMETIC OPERATORS 
+// Addition Operator examples 
+let a = 7 
+let b = 3 
+let word1 = 'Hello '
+let word2 = 'James'
+console.log(a + b);
+
+
+// subtraction operator 
+console.log(a - b)
+console.log('James Mentor'.length -3)
+
+// assignment operator 
+let x = 235  
+y = x 
+console.log(y)
+
+// addition assignment operator
+x += 3  
+sentence = word2 += ' How are you?'
+console.log (sentence)
+
+// Comparison Operators
+// Equality operator
+
+let num= 2
+let str = '2'
+ num == str;
+
+'venecia' == 'kakusha'
+//Inequality operator(!=) 
+ 2 != '2' ;
+'venecia' != 'Kakusha' 
+
+// Logical Operators 
+// Logical AND operator 
+let myName = "Venecia";
+let age = 25;
+if (myName === "Venecia" && age === 30) {
+  console.log("My Name is Ven and I love that I am turning 30.");
+}else{
+    console.log("This is unfortunate")
+}
+
+1 < 2 && 'hello' == 'hello'
+// Logical OR operator 
+true || false 
+1 > 2 || 'hello' == 'world' 
+// Bitwise AND operator
+5 & 3 ;
+// returns 1,
+ 13 & 7 
+//  returns 5.
+
+// Bitwise OR operator
+5 | 3 
+// returns 7, 
+13 | 7 
+// returns 15.
+
+
+
+// // Question 3
+// // // Define the class group
+// let classGroup = "Arts";
+
+// // Check the class group and output the subjects accordingly
+// if (classGroup === "Science") {
+//   console.log("Physics, Chemistry, Biology, Technical Drawing, English, Mathematics");
+// } else if (classGroup === "Social Science") {
+//   console.log("Accounting, Commerce, Marketing, Geography, English, Mathematics");
+// } else if (classGroup === "Arts") {
+//   console.log("Government, Economics, Literature, History, English, Mathematics");
+// } else {
+//   console.log(" English, Mathematics");
+// }
+
+
+
+// // // Question 4
+// // 1, 8 , 15 
+
+
+
+
+// // Question 5 
+// // Define the input number
+// const num = 3;
+// // Initialize the power of 2 and the difference variables
+// let pwr = 1;
+// let diff = Math.abs(num - pwr);
+// // Loop through the powers of 2 until the difference is not decreasing
+// for (let i = 2; diff >= Math.abs(num - i); i *= 2) {
+//   pwr = i;
+//   diff = Math.abs(num - pwr);
+// }
+// // check for a smaller value
+// if (pwr > 1 && diff > Math.abs(num - pwr/2)) {
+//   pwr /= 2;
+// }
+// console.log(`The number ${pwr} is the power of 2 nearest to ${num}.`)
---
+++ readme.md
@@ -0,0 +1,35 @@
+### List the symbols for each of the below Javascript operators. For each JavaScript Operator, write 2 examples.
+- Arithmetic Operators
+##### Addition operator(+)
+##### Subtraction operator(-)
+##### Assignment Operators (=)
+##### Addition assignment operator(+=) 
+- Comparison Operators
+##### Equality operator(==) 
+##### Inequality operator(!=) 
+- Logical Operators
+##### Logical AND operator(&&)
+##### Logical OR operator(||) 
+- Bitwise Operators
+##### Bitwise AND operator(&)
+##### Bitwise OR operator(|)
+
+ ## Question 3 
+### Bolatito just got admitted to Greenfield High School as an Arts student. To commence her studies, she needs to know the subjects she will be taking as an Arts student. All students have to take the General subjects.The subjects for each class group are as follows:
+	- Science Subjects - Physics, Chemistry, Biology, Technical Drawing 
+    - Social Science Subjects - Accounting, Commerce, Marketing, Geography
+    - Arts Subjects - Government, Economics, Literature, History
+    - General Subjects - English, Mathematics
+
+### Using if…else conditional statement, write a program that will help Bolatito determine the subjects she will be taking for the session using her class group. Also, in the case of an invalid class group, your output should be the General subjects. 
+
+
+## Question 4 
+### What is the result when the following program is executed?
+- for (let i = 1; i < 20; i += 7) {console.log(i);}
+
+## Question 5 
+### Write a program that takes a positive number (num) and finds the power of 2 nearest to that number. The program stores the resulting value to pwr. Then you log the answer in this format: 
+- The number (pwr) is the power of 2 nearest to (num).
+### If there are two candidate values, display the smaller one. For example, 2 and 4 4 are both the powers of 2 nearest to 3. The function should display 2 because it’s smaller than 4.
+
